08cc730276816a42434ef456428b8507a3485b9b
Lojain Banishamsa <s12116190@stu.najah.edu>
2025-05-20 22:03:47 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+// App.js
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import Home from './pages/Home';
+
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 function App({ signOut, user }) {
   return (
-    <div>
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
-    </div>
+    <>
+      <div className="d-flex justify-content-end p-2">
+        <span className="me-3">Welcome, {user.username}</span>
+        <button className="btn btn-outline-danger" onClick={signOut}>
+          Sign Out
+        </button>
+      </div>
+      <Home />
+    </>
   );
 }
 
